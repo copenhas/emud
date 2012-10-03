5dcb7f645a84eea3d88f5a34484d19a18a270e0f
Sean Copenhaver <sean.copenhaver@gmail.com>
2012-10-02 21:00:12 -0400

    tests pass again

--- lib/emud/test/emud_sess_tests.erl
+++ lib/emud/test/emud_sess_tests.erl
@@ -80,7 +80,7 @@ can_execute_a_cmd_and_recieve_msg_test() ->
     Look = #cmd{type=look, sessid = SessId},
     ?assertEqual(ok, emud_sess:handle_cmd(Sess, Look)),
     receive 
-        {emud_msg, _Ref, Msg} -> ?assertMatch(#msg{type=look}, Msg)
+        {emud_msg, _Ref, #msg{type=look}} -> ok
     after 1000 ->
         throw(no_msg_found)
     end,
@@ -89,10 +89,9 @@ can_execute_a_cmd_and_recieve_msg_test() ->
 
 start_session() ->
     helper:start_deps(),
-    application:start(emud),
     {ok, SessId, Sess} = emud_srv:connect(),
     receive 
-        {emud_msg, _Ref, Msg} -> ?assertMatch(#msg{type=welcome}, Msg)
+        {emud_msg, _Ref, #msg{type=welcome}} -> ok
     after 1000 ->
         throw(no_msg_found)
     end,
